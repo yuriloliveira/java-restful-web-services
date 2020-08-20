7b6a1cc3089cc089d6d2669575a079ee4880dcc7
Yuri Oliveira <yuri12194@gmail.com>
2020-08-20 18:39:24 -0300

    step 22.

--- src/main/java/com/rest/webservices/restfulwebservices/user/User.java
+++ src/main/java/com/rest/webservices/restfulwebservices/user/User.java
@@ -5,13 +5,19 @@ import java.util.Date;
 import javax.validation.constraints.Past;
 import javax.validation.constraints.Size;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+
+@ApiModel(description="All details about user.")
 public class User {
 	private Integer id;
 	
 	@Size(min=2, message="Name should have at least 2 characters")
+	@ApiModelProperty(notes="Name should have at least 2 characters")
 	private String name;
 	
 	@Past(message="Birthdate should be in the past")
+	@ApiModelProperty(notes="Birthdate should be in the past")
 	private Date birthDate;
 	
 	public User(Integer id, String name, Date birthDate) {
