ab36b350e9315a2b5c4c3988fe9a1a95fc402816
Yuri Oliveira <yuri12194@gmail.com>
2020-08-20 17:59:32 -0300

    step 18 - part 2.

--- src/main/java/com/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
+++ src/main/java/com/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
@@ -4,9 +4,9 @@ import java.util.Locale;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
 // Controller
@@ -32,7 +32,7 @@ public class HelloWorldController {
 	}
 	
 	@GetMapping(path="/hello-world-internationalized")
-	public String helloWorldInternationalized(@RequestHeader(name="Accept-Language", required=false) Locale locale) {
-		return this.messageSource.getMessage("good.morning.message", null, locale);
+	public String helloWorldInternationalized() {
+		return this.messageSource.getMessage("good.morning.message", null, LocaleContextHolder.getLocale());
 	}
  }
