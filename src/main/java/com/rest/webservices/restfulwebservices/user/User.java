d6d11faf8eaf8a37c1c745b6b175efcfea18b637
Yuri Oliveira <yuri12194@gmail.com>
2020-08-21 15:04:21 -0300

    step 30.

--- src/main/java/com/rest/webservices/restfulwebservices/user/User.java
+++ src/main/java/com/rest/webservices/restfulwebservices/user/User.java
@@ -2,6 +2,9 @@ package com.rest.webservices.restfulwebservices.user;
 
 import java.util.Date;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 import javax.validation.constraints.Past;
 import javax.validation.constraints.Size;
 
@@ -9,7 +12,10 @@ import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 
 @ApiModel(description="All details about user.")
+@Entity
 public class User {
+	@Id
+	@GeneratedValue
 	private Integer id;
 	
 	@Size(min=2, message="Name should have at least 2 characters")
