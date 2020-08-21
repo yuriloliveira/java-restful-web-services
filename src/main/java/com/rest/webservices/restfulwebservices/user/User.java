f7d1cda3bc9179d05a6914fe99a902a17f136ae0
Yuri Oliveira <yuri12194@gmail.com>
2020-08-21 16:05:40 -0300

    step 33 (Post entity).

--- src/main/java/com/rest/webservices/restfulwebservices/user/User.java
+++ src/main/java/com/rest/webservices/restfulwebservices/user/User.java
@@ -1,10 +1,12 @@
 package com.rest.webservices.restfulwebservices.user;
 
 import java.util.Date;
+import java.util.List;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.validation.constraints.Past;
 import javax.validation.constraints.Size;
 
@@ -25,6 +27,9 @@ public class User {
 	@Past(message="Birthdate should be in the past")
 	@ApiModelProperty(notes="Birthdate should be in the past")
 	private Date birthDate;
+	
+	@OneToMany(mappedBy="user")
+	private List<Post> posts;
 	
 	public User() {
 		
@@ -61,6 +66,14 @@ public class User {
 		this.birthDate = birthDate;
 	}
 	
+	public List<Post> getPosts() {
+		return posts;
+	}
+
+	public void setPosts(List<Post> posts) {
+		this.posts = posts;
+	}
+
 	@Override
 	public String toString() {
 		return "User [id=" + id + ", name=" + name + ", birthDate=" + birthDate + "]";
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,5 @@
 insert into user values (10001, sysdate(), 'Joao de Santo Cristo');
 insert into user values (10002, sysdate(), 'Barney o Dinossauro');
 insert into user values (10003, sysdate(), 'Lindomar');
+insert into post values (10001, 'My First Post', 10001);
+insert into post values (10002, 'My Second Post', 10001);
